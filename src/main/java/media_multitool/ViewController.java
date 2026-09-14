6f45290eddcd8557c7e9b8b69023f1ac4f811aae
Jussi6 <artyrkorolyov@gmail.com>
2026-09-14 21:32:42 +0300

    feat(navigation): replace combo-box menus with collapsible sidebar folders and svg icons

--- src/main/java/media_multitool/ViewController.java
+++ src/main/java/media_multitool/ViewController.java
@@ -1,17 +1,19 @@
 package media_multitool;
 
-import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import model.utility.Item;
-import viewHelp.ComboBoxes;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class ViewController {
 
@@ -26,105 +28,22 @@ public class ViewController {
     @FXML private Button navHomeButton, navConverterImageButton, navConverterVideoButton, navConverterAudioButton,
             navCompressorImage, navCompressorVideo, navInfo, navEditorAudioTag;
 
-    @FXML private ComboBox<Item> comboBoxChoiceActionImage, comboBoxChoiceActionPdf, comboBoxChoiceActionWatermark;
-
-    /* index page image
-      1, negativeImagePage,
-      2, rotateImagePage,
-      3, lightenPage,
-      4, darkenPage,
-      5, colorizePage,
-      6, blackAndWhitePage,
-      7, blurPage,
-      8 spotBlurPage,
-      9  findPixelPage,
-      10 colorReplaceInImagePage,
-      11  cropPage,
-      */
-
-    /* index page pdf
-       21, imageToPdfPage,
-       22, pdfToImagePage,
-       23, imagesToPdfPage,
-       24, protectPdfPage,
-       25, unlockPdfPage,
-       26, removePagesPdfPage,
-       27, compressPdfPage,
-       28, mergePdfPage,
-       29, splitPdfPage
-       */
-
-    /* index page watermark
-       41, watermarkImagePane,
-       42, watermarkPdfPane,
-       43, watermarkVideoPane
-     */
+    @FXML private VBox imageToolsContent, pdfToolsContent, watermarkContent;
+    @FXML private ToggleButton btnConverters, btnCompressors, btnImageTools, btnPdfTools, btnWatermark;
 
     private final Map<Integer, StackPane> stackPaneMapImageTools = new HashMap<>();
     private final Map<Integer, StackPane> stackPaneMapPdfTools = new HashMap<>();
     private final Map<Integer, StackPane> stackPaneMapWatermark = new HashMap<>();
 
-    private List<Button> listNavBtn;
+    private final List<Button> listSubBtn = new ArrayList<>();
 
     @FXML
     public void initialize() {
-        listNavBtn = List.of(navHomeButton, navConverterImageButton, navConverterVideoButton, navConverterAudioButton,
-                navCompressorImage, navCompressorVideo, navInfo, navEditorAudioTag);
+        listSubBtn.addAll(List.of(navConverterImageButton, navConverterVideoButton, navConverterAudioButton, navCompressorImage, navCompressorVideo));
 
         if (homeViewController != null) {
             homeViewController.setMainController(this);
         }
-
-        showHomePage();
-
-        // Image
-        comboBoxChoiceActionImage.setPromptText("Image tools");
-        ComboBoxes.setupComboBox(comboBoxChoiceActionImage, Item::title);
-        comboBoxChoiceActionImage.getSelectionModel().clearSelection();
-        comboBoxChoiceActionImage.buttonCellProperty().bind(Bindings.createObjectBinding(() -> new ListCell<Item>() {
-            @Override
-            protected void updateItem(Item item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty || item == null) {
-                    setText("Image tools");
-                } else {
-                    setText(item.title());
-                }
-            }
-        }, comboBoxChoiceActionImage.getSelectionModel().selectedItemProperty()));
-        comboBoxChoiceActionImage.setVisibleRowCount(10);
-
-        // PDF
-        comboBoxChoiceActionPdf.setPromptText("PDF tools");
-        ComboBoxes.setupComboBox(comboBoxChoiceActionPdf, Item::title);
-        comboBoxChoiceActionPdf.getSelectionModel().clearSelection();
-        comboBoxChoiceActionPdf.buttonCellProperty().bind(Bindings.createObjectBinding(() -> new ListCell<Item>() {
-            @Override
-            protected void updateItem(Item item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty || item == null) {
-                    setText("PDF tools");
-                } else {
-                    setText(item.title());
-                }
-            }
-        }, comboBoxChoiceActionPdf.getSelectionModel().selectedItemProperty()));
-
-        // Watermark
-        comboBoxChoiceActionWatermark.setPromptText("Watermark");
-        ComboBoxes.setupComboBox(comboBoxChoiceActionWatermark, Item::title);
-        comboBoxChoiceActionWatermark.getSelectionModel().clearSelection();
-        comboBoxChoiceActionWatermark.buttonCellProperty().bind(Bindings.createObjectBinding(() -> new javafx.scene.control.ListCell<Item>() {
-            @Override
-            protected void updateItem(Item item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty || item == null) {
-                    setText("Watermark");
-                } else {
-                    setText(item.title());
-                }
-            }
-        }, comboBoxChoiceActionWatermark.getSelectionModel().selectedItemProperty()));
 
         stackPaneMapImageTools.putAll(Map.ofEntries(
                 Map.entry(1,  negativeImagePage),
@@ -138,7 +57,6 @@ public class ViewController {
                 Map.entry(9,  findPixelPage),
                 Map.entry(10, colorReplaceInImagePage),
                 Map.entry(11, cropPage)
-
         ));
 
         stackPaneMapPdfTools.putAll(Map.of(
@@ -159,13 +77,7 @@ public class ViewController {
                 43, watermarkVideoPane
         ));
 
-        comboBoxChoiceActionWatermark.getItems().addAll(
-                new Item(41, "Watermark Image"),
-                new Item(42, "Watermark PDF"),
-                new Item(43, "Watermark Video")
-        );
-
-        comboBoxChoiceActionImage.getItems().addAll(
+        populateVBox(imageToolsContent, List.of(
                 new Item(1, "Negative photo"),
                 new Item(2, "Rotate photo"),
                 new Item(3, "Lighten photo"),
@@ -177,9 +89,9 @@ public class ViewController {
                 new Item(9, "Find Pixel"),
                 new Item(10, "Color Replace"),
                 new Item(11, "Crop Image")
-        );
+        ), this::onActionChoiceActionImage);
 
-        comboBoxChoiceActionPdf.getItems().addAll(
+        populateVBox(pdfToolsContent, List.of(
                 new Item(21, "Image To PDF"),
                 new Item(22, "PDF To Image"),
                 new Item(23, "Image(s) To PDF"),
@@ -189,120 +101,136 @@ public class ViewController {
                 new Item(27, "Compress PDF"),
                 new Item(28, "Merge PDF"),
                 new Item(29, "Split PDF")
-        );
+        ), this::onActionChoiceActionPdf);
+
+        populateVBox(watermarkContent, List.of(
+                new Item(41, "Watermark Image"),
+                new Item(42, "Watermark PDF"),
+                new Item(43, "Watermark Video")
+        ), this::onActionChoiceActionWatermark);
+
+        showHomePage();
+    }
+
+    private void populateVBox(VBox box, List<Item> items, Consumer<Integer> action) {
+        for (Item item : items) {
+            Button btn = new Button(item.title());
+            btn.getStyleClass().add("sub-btn");
+            btn.setPrefWidth(150);
+            btn.setAlignment(Pos.BASELINE_LEFT);
+
+            btn.setOnAction(_ -> {
+                action.accept((int) item.id());
+                setActiveSubButton(btn);
+            });
+
+            btn.setUserData((int) item.id());
+            box.getChildren().add(btn);
+            listSubBtn.add(btn);
+        }
     }
 
     @FXML
     public void showHomePage() {
         setActivePage(homeView, navHomeButton);
+        if (btnConverters != null && btnConverters.getToggleGroup() != null) btnConverters.getToggleGroup().selectToggle(null);
     }
 
     @FXML
     public void showConverterImagePage() {
-        setActivePage(converterImagePage, navConverterImageButton);
+        setActivePage(converterImagePage, null);
+        setActiveSubButton(navConverterImageButton);
+        btnConverters.setSelected(true);
     }
 
     @FXML
     public void showConverterVideoPage(){
-        setActivePage(converterVideoPage, navConverterVideoButton);
+        setActivePage(converterVideoPage, null);
+        setActiveSubButton(navConverterVideoButton);
+        btnConverters.setSelected(true);
     }
 
     @FXML
     public void showConverterAudioPage() {
-        setActivePage(converterMP3Page, navConverterAudioButton);
+        setActivePage(converterMP3Page, null);
+        setActiveSubButton(navConverterAudioButton);
+        btnConverters.setSelected(true);
     }
 
     @FXML
     public void showCompressorImagePage() {
-        setActivePage(compressorImagePage, navCompressorImage);
+        setActivePage(compressorImagePage, null);
+        setActiveSubButton(navCompressorImage);
+        btnCompressors.setSelected(true);
     }
 
     @FXML
     public void showCompressorVideoPage() {
-        setActivePage(compressorVideoPage, navCompressorVideo);
+        setActivePage(compressorVideoPage, null);
+        setActiveSubButton(navCompressorVideo);
+        btnCompressors.setSelected(true);
     }
 
     @FXML
     public void showEditorAudioTagPage() {
         setActivePage(audioEditorTagPage, navEditorAudioTag);
-    }
-
-    @FXML
-    public void onActionChoiceActionPdf() {
-        Item selectedItem = comboBoxChoiceActionPdf.getValue();
-        if (selectedItem != null) {
-            currentPageFromComboBoxAction = stackPaneMapPdfTools.get((int) selectedItem.id());
-            setActivePage(currentPageFromComboBoxAction, null);
-            comboBoxChoiceActionPdf.setStyle(getComboBoxStyle(true));
-        }
+        if (btnConverters != null && btnConverters.getToggleGroup() != null) btnConverters.getToggleGroup().selectToggle(null);
     }
 
     @FXML
     public void onActionChoiceActionPdf(int index) {
         currentPageFromComboBoxAction = stackPaneMapPdfTools.get(index);
         setActivePage(currentPageFromComboBoxAction, null);
-        comboBoxChoiceActionImage.getSelectionModel().clearSelection();
-        
-        comboBoxChoiceActionPdf.getItems().stream()
-                .filter(item -> item.id() == index)
-                .findFirst()
-                .ifPresent(item -> comboBoxChoiceActionPdf.getSelectionModel().select(item));
-        
-        comboBoxChoiceActionPdf.setStyle(getComboBoxStyle(true));
-    }
-
-    @FXML
-    public void onActionChoiceActionWatermark() {
-        Item selectedItem = comboBoxChoiceActionWatermark.getValue();
-        if (selectedItem != null) {
-            currentPageFromComboBoxAction = stackPaneMapWatermark.get((int) selectedItem.id());
-            setActivePage(currentPageFromComboBoxAction, null);
-            comboBoxChoiceActionWatermark.setStyle(getComboBoxStyle(true));
-        }
+        btnPdfTools.setSelected(true);
+        highlightSubButtonByUserData(pdfToolsContent, index);
     }
 
     @FXML
     public void onActionChoiceActionWatermark(int index) {
         currentPageFromComboBoxAction = stackPaneMapWatermark.get(index);
         setActivePage(currentPageFromComboBoxAction, null);
-        comboBoxChoiceActionWatermark.getSelectionModel().clearSelection();
-
-        comboBoxChoiceActionWatermark.getItems().stream()
-                .filter(item -> item.id() == index)
-                .findFirst()
-                .ifPresent(item -> comboBoxChoiceActionWatermark.getSelectionModel().select(item));
-
-        comboBoxChoiceActionWatermark.setStyle(getComboBoxStyle(true));
-    }
-
-    @FXML
-    public void onActionChoiceActionImage() {
-        Item selectedItem = comboBoxChoiceActionImage.getValue();
-        if (selectedItem != null) {
-            currentPageFromComboBoxAction = stackPaneMapImageTools.get((int) selectedItem.id());
-            setActivePage(currentPageFromComboBoxAction, null);
-            comboBoxChoiceActionPdf.getSelectionModel().clearSelection();
-            comboBoxChoiceActionImage.setStyle(getComboBoxStyle(true));
-        }
+        btnWatermark.setSelected(true);
+        highlightSubButtonByUserData(watermarkContent, index);
     }
 
     @FXML
     public void onActionChoiceActionImage(int index) {
-            currentPageFromComboBoxAction = stackPaneMapImageTools.get(index);
-            setActivePage(currentPageFromComboBoxAction, null);
-            comboBoxChoiceActionPdf.getSelectionModel().clearSelection();
-            
-            comboBoxChoiceActionImage.getItems().stream()
-                    .filter(item -> item.id() == index)
-                    .findFirst()
-                    .ifPresent(item -> comboBoxChoiceActionImage.getSelectionModel().select(item));
-            
-            comboBoxChoiceActionImage.setStyle(getComboBoxStyle(true));
+        currentPageFromComboBoxAction = stackPaneMapImageTools.get(index);
+        setActivePage(currentPageFromComboBoxAction, null);
+        btnImageTools.setSelected(true);
+        highlightSubButtonByUserData(imageToolsContent, index);
+    }
+
+    private void highlightSubButtonByUserData(VBox box, int index) {
+        for (Node node : box.getChildren()) {
+            if (node instanceof Button btn) {
+                if (btn.getUserData() != null && (int) btn.getUserData() == index) {
+                    setActiveSubButton(btn);
+                    break;
+                }
+            }
+        }
     }
 
     @FXML
     public void showInfoPage() {
         setActivePage(infoPage, navInfo);
+        if (btnConverters != null && btnConverters.getToggleGroup() != null) btnConverters.getToggleGroup().selectToggle(null);
+    }
+
+    private void setActiveSubButton(Button activeBtn) {
+        for (Button btn : listSubBtn) {
+            btn.getStyleClass().remove("sub-btn-active");
+        }
+        if (activeBtn != null) {
+            if (!activeBtn.getStyleClass().contains("sub-btn-active")) {
+                activeBtn.getStyleClass().add("sub-btn-active");
+            }
+            if (activeBtn == navHomeButton || activeBtn == navEditorAudioTag || activeBtn == navInfo) {
+                activeBtn.getStyleClass().remove("standalone-btn-active");
+                activeBtn.getStyleClass().add("standalone-btn-active");
+            }
+        }
     }
 
     private void setActivePage(StackPane pageToShow, Button activeButton) {
@@ -323,30 +251,13 @@ public class ViewController {
             }
         }
 
-        comboBoxChoiceActionImage.setStyle(getComboBoxStyle(false));
-        comboBoxChoiceActionPdf.setStyle(getComboBoxStyle(false));
-        comboBoxChoiceActionWatermark.setStyle(getComboBoxStyle(false));
+        navHomeButton.getStyleClass().remove("standalone-btn-active");
+        navEditorAudioTag.getStyleClass().remove("standalone-btn-active");
+        navInfo.getStyleClass().remove("standalone-btn-active");
 
         if (activeButton != null) {
-            comboBoxChoiceActionImage.getSelectionModel().clearSelection();
-            comboBoxChoiceActionPdf.getSelectionModel().clearSelection();
-            comboBoxChoiceActionWatermark.getSelectionModel().clearSelection();
+            activeButton.getStyleClass().add("standalone-btn-active");
+            setActiveSubButton(null);
         }
-
-        listNavBtn.forEach(btn -> btn.setStyle(getNavButtonStyle(activeButton == btn)));
-    }
-
-    private String getNavButtonStyle(boolean active) {
-        if (active) {
-            return "-fx-background-color: #32CD32; -fx-text-fill: black; -fx-font-weight: bold; -fx-background-radius: 8;";
-        }
-        return "-fx-background-color: #323232; -fx-text-fill: white; -fx-background-radius: 8;";
-    }
-
-    private String getComboBoxStyle(boolean active) {
-        if (active) {
-            return "-fx-background-color: #32CD32; -fx-background-radius: 8; -fx-font-weight: bold;";
-        }
-        return "-fx-background-color: #323232; -fx-background-radius: 8;";
     }
 }
